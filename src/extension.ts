4d83d81064f5f6a2e026275e7dea3a4eee065a66
Josa Gesell <josa@gesell.me>
2017-05-10 16:44:52 +0200

    ✨  Respect files.exclude settings

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,13 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as minimatch from 'minimatch';
 
-function dirContent(rootPath: string, dirPath: string) : Promise<string[]> {
+function testPatterns(path, patterns) {
+  return !patterns.find((pattern) => minimatch(path, pattern));
+}
+
+function dirContent(rootPath: string, dirPath: string, ignorePatterns: string[]) : Promise<string[]> {
   return new Promise((resolve: Function, reject: Function) => {
 
     const basePath = path.join(rootPath, dirPath);
@@ -13,6 +18,7 @@ function dirContent(rootPath: string, dirPath: string) : Promise<string[]> {
     fs.readdir(basePath, (err, files) => {
       if (err) { return reject(err); }
       resolve(files
+        .filter((name) => testPatterns(path.join(dirPath, name), ignorePatterns))
         .map((name) => {
           name = path.join(dirPath, name)
           const stats = fs.statSync(path.join(rootPath, name))
@@ -37,13 +43,13 @@ function dirContent(rootPath: string, dirPath: string) : Promise<string[]> {
   })
 }
 
-function showDirContent(rootPath: string, dirPath: string) {
+function showDirContent(rootPath: string, dirPath: string, ignorePatterns: string[]) {
 
   if (dirPath === '.') {
     dirPath = ''
   }
 
-  dirContent(rootPath, dirPath)
+  dirContent(rootPath, dirPath, ignorePatterns)
     .then((paths) => {
 
       const options = !dirPath ? paths : ['..', ...paths];
@@ -54,9 +60,9 @@ function showDirContent(rootPath: string, dirPath: string) {
       if (!selection) { return; }
 
       if (selection === '..') {
-        showDirContent(rootPath, path.join(dirPath, selection))
+        showDirContent(rootPath, path.join(dirPath, selection), ignorePatterns)
       } else if (selection.match(/\/$/)) {
-        showDirContent(rootPath, selection)
+        showDirContent(rootPath, selection, ignorePatterns)
       } else {
         openFile(path.join(rootPath, selection))
       }
@@ -70,14 +76,23 @@ function openFile(filePath: string) {
     // .catch((error) => vscode.window.showErrorMessage(error.message))
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function ignores() {
+  const configFilesExclude =
+    vscode.workspace.getConfiguration('files.exclude');
+
+  const workspaceIgnored = Object.keys(configFilesExclude)
+    .filter(key => configFilesExclude[key]);
+
+  return workspaceIgnored;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand('extension.showFileNavigator', () => {
     const { rootPath } = vscode.workspace;
 
     let dirPath = '';
+    let ignorePatterns = ignores();
     if (vscode.window.activeTextEditor) {
       const { fileName } = vscode.window.activeTextEditor.document
       if (fileName.indexOf(rootPath) === 0) {
@@ -85,10 +100,10 @@ export function activate(context: vscode.ExtensionContext) {
       }
     }
 
-    showDirContent(rootPath, dirPath)
+    showDirContent(rootPath, dirPath, ignorePatterns)
   });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
